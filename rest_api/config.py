76267d2d8c660d03152a81dce9baf1fd5b6c948a
Estifanos <estifanosbeyene21@gmail.com>
2023-03-24 15:05:31 -0400

    feat: create config class

--- rest_api/config.py
+++ rest_api/config.py
@@ -1,15 +1,29 @@
 import os
-
-SECRET_KEY = os.urandom(32)
-
-# Grabs the folder where the script runs.
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-# Enable debug mode.
-DEBUG = True
 
-# Connect to the database
-SQLALCHEMY_DATABASE_URI = 'your psycopg2 URI connection'
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'this-really-needs-to-be-changed'
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
 
-# Turn off the Flask-SQLAlchemy event system and warning
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
